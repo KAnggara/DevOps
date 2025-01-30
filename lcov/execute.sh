c9a455213d6d78ae537ebd3c9d96d6ef23172f4c
Call Vin <kanggara75@gmail.com>
2025-01-30 10:51:13 +0700

    fix: execute_lcov: command not found

--- lcov/execute.sh
+++ lcov/execute.sh
@@ -7,21 +7,13 @@ abort() {
 }
 
 execute_lcov() {
-	pwd
-	ls -la
 	mkdir $GITHUB_WORKSPACE/$OUTDIR
-	ls -la
 	cd $WD
-	pwd
 	genhtml $COVERAGE_FILE --rc branch_coverage=1 -o $GITHUB_WORKSPACE/$OUTDIR
-	ls -la
-	cd $GITHUB_WORKSPACE/$OUTDIR
-	ls -la
-	pwd
 }
 
 main() {
 	execute_lcov
 }
 
-main || abort "LCOV Setup Error!"
+main || abort "LCOV Execute Error!"
