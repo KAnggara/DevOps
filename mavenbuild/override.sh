064d993b872d1bbe0113ca3d77e9e81e0be56274
Call Vin <kanggara75@gmail.com>
2025-02-02 00:16:43 +0700

    test maven build 2

---
+++ mavenbuild/override.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eu
+
+abort() {
+	printf "%s\n" "$@"
+	exit 1
+}
+
+override_config() {
+	if [[ -z "$VAR" ]]; then
+		echo "Variabel kosong"
+	else
+		echo "Variabel tidak kosong: $APPLICATION_PROPERTIES"
+	fi
+	pwd
+	ls -la
+}
+
+main() {
+	override_config
+}
+
+main || abort "override config Execute Error!"
