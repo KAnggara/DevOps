9813386294253a3e50a3e92b59b4f2f54adc95ab
Call Vin <kanggara75@gmail.com>
2025-02-02 10:40:09 +0700

    test mavenbuild with var 5

--- mavenbuild/mavenbuild.sh
+++ mavenbuild/mavenbuild.sh
@@ -7,6 +7,12 @@ abort() {
 }
 
 override_config() {
+	pwd
+	if [[ -n "$WORK_DIR" ]]; then
+		cd $WORK_DIR
+	fi
+	pwd
+
 	if [[ -n "$APPLICATION_PROPERTIES" ]]; then
 		if [[ -f src/main/resources/application.properties ]]; then
 			printf "%s" "$APPLICATION_PROPERTIES" >src/main/resources/application.properties
@@ -15,11 +21,23 @@ override_config() {
 		else
 			abort "Error: application.yaml or application.properties not found!"
 		fi
+	else
+		echo "APPLICATION_PROPERTIES var not exist, keep it as is"
 	fi
+
+}
+
+maven_build() {
+	echo "MVN Build"
+	pwd
+	mvn --batch-mode --update-snapshots verify
+	mkdir $GITHUB_WORKSPACE/$WORK_DIR/staging
+	cp target/*.jar $GITHUB_WORKSPACE/staging
 }
 
 main() {
 	override_config
+	maven_build
 }
 
 main || abort "override config Execute Error!"
