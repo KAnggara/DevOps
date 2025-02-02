cfe95e483d4ea254d8a6b66760a0def300781715
Call Vin <kanggara75@gmail.com>
2025-02-02 15:40:11 +0700

    fix Sun Feb  2 15:40:11 WIB 2025

--- test/bun/src/calc.ts
+++ test/bun/src/calc.ts
@@ -1,17 +1,15 @@
 export class Calc {
+  static add(a: number, b: number) {
+    return a + b;
+  }
+  static div(a: number, b: number) {
+    return a - b;
+  }
   static sum(...numbers: number[]) {
     let sum = 0;
     for (const element of numbers) {
       sum += element;
     }
     return sum;
-  }
-
-  static add(a: number, b: number) {
-    return a + b;
-  }
-
-  static div(a: number, b: number) {
-    return a - b;
   }
 }
