d5bea7bb2e8b89f14e9074fdd48078dc6705c03d
Call Vin <kanggara75@gmail.com>
2025-02-02 15:05:34 +0700

    feat: docker build and publish

---
+++ dockerbuild/build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -eu
+
+abort() {
+	printf "%s\n" "$@"
+	exit 1
+}
+
+override_config() {
+	if [[ -n "$WORK_DIR" ]]; then
+		cd $WORK_DIR
+	fi
+
+	if [[ -n "$APPLICATION_PROPERTIES" ]]; then
+		if [[ -f src/main/resources/application.properties ]]; then
+			echo "Rewrite application.properties"
+			printf "%s" "$APPLICATION_PROPERTIES" >src/main/resources/application.properties
+		elif [[ -f src/main/resources/application.yaml ]]; then
+			echo "Rewrite application.yaml"
+			printf "%s" "$APPLICATION_PROPERTIES" >src/main/resources/application.yaml
+		elif [[ -f src/main/resources/application.yml ]]; then
+			echo "Rewrite application.yml"
+			printf "%s" "$APPLICATION_PROPERTIES" >src/main/resources/application.yml
+		else
+			abort "Error: application.yaml/yml or application.properties not found!"
+		fi
+	else
+		echo "APPLICATION_PROPERTIES var not exist, keep it as is"
+	fi
+
+}
+
+maven_build() {
+	echo "MVN Build"
+	mvn --batch-mode --update-snapshots verify
+	mkdir $GITHUB_WORKSPACE/mavenbuild
+	cp target/*.jar $GITHUB_WORKSPACE/mavenbuild
+}
+
+main() {
+	override_config
+	maven_build
+}
+
+main || abort "override config Execute Error!"
