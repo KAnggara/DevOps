60b612a0c23f21af3619403ba4664902bc6521ab
Call Vin <kanggara75@gmail.com>
2025-02-02 11:02:46 +0700

    fix

--- mavenbuild/mavenbuild.sh
+++ mavenbuild/mavenbuild.sh
@@ -15,11 +15,16 @@ override_config() {
 
 	if [[ -n "$APPLICATION_PROPERTIES" ]]; then
 		if [[ -f src/main/resources/application.properties ]]; then
+			echo "Rewrite application.properties"
 			printf "%s" "$APPLICATION_PROPERTIES" >src/main/resources/application.properties
 		elif [[ -f src/main/resources/application.yaml ]]; then
+			echo "Rewrite application.yaml"
 			printf "%s" "$APPLICATION_PROPERTIES" >src/main/resources/application.yaml
+		elif [[ -f src/main/resources/application.yml ]]; then
+			echo "Rewrite application.yml"
+			printf "%s" "$APPLICATION_PROPERTIES" >src/main/resources/application.yml
 		else
-			abort "Error: application.yaml or application.properties not found!"
+			abort "Error: application.yaml/yml or application.properties not found!"
 		fi
 	else
 		echo "APPLICATION_PROPERTIES var not exist, keep it as is"
@@ -30,19 +35,23 @@ override_config() {
 maven_build() {
 	echo "MVN Build"
 	pwd
+	ls -la
+
 	mvn --batch-mode --update-snapshots verify
 	ls -la
+
 	mkdir $GITHUB_WORKSPACE/staging
 	echo $GITHUB_WORKSPACE
 	ls -la
+
+	ls -la $GITHUB_WORKSPACE/staging
 	cp target/*.jar $GITHUB_WORKSPACE/staging
 	cd $GITHUB_WORKSPACE/staging
+	pwd
 	ls -la
 }
 
 main() {
-	pwd
-	cd $GITHUB_WORKSPACE
 	override_config
 	maven_build
 }
