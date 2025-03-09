69b62940ccd70f4657bfc952bc5569cc52e31de4
Call Vin <kanggara75@gmail.com>
2025-03-09 10:07:31 +0700

    new bun test

---
+++ test/bun/buntest.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -eu
+
+abort() {
+	printf "%s\n" "$@"
+	exit 1
+}
+
+override_config() {
+	if [[ -n "$WORK_DIR" ]]; then
+		cd $WORK_DIR
+	fi
+
+	if [[ -n "$DOTENV" ]]; then
+		if [[ -f .env ]]; then
+			echo "Rewrite .env"
+			printf "%s" "$DOTENV" >.env
+		elif [[ -f env.example ]]; then
+			echo "Rewrite env.example to .env"
+			cp env.example .env
+		fi
+	elif [[ -f .env ]]; then
+		echo "DOTENV var not exist, keep it as is"
+	elif [[ -f env.example ]]; then
+		echo "Rewrite env.example to .env"
+		cp env.example .env
+	else
+		echo "DOTENV var not exist, keep it as is"
+	fi
+}
+
+bun_test() {
+	echo "Bun Test"
+	bun -v
+	bun install
+	bun test
+}
+
+main() {
+	override_config
+	bun_test
+}
+
+main || abort "Override config Execute Error!"
